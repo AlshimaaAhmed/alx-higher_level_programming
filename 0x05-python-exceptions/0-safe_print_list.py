56214a88d977576d8b24234902558bc92316da8d
AlshimaaAhmed <alshimaa185@gmail.com>
2024-02-05 16:17:52 +0200

    new

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+        try:
+            counter = 0
+            while counter < x :
+                print("{:d}".format(my_list[counter]), end="")
+                counter += 1
+            print()
+            return counter
+        except IndexError:
+             print()
+             return counter
+          
