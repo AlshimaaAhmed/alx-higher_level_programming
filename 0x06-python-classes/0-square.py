049c52e125407c835bd2077531eec93d3f8815c9
AlshimaaAhmed <alshimaa185@gmail.com>
2024-02-10 14:41:41 +0200

    new

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
+""" square module """
 class square :
-     """
-    A class representing XYZ entity.
-
-    This class provides methods for interacting with XYZ objects.
-
-    Attributes:
-        no attribute
-
-    Methods:
-        no methodes
-    """
-     def __init__(self) -> None:
-        pass
+ """ square class """
+ pass
