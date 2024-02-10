ea48dd0c1091a7a0eea16d1228d74dba331d091e
AlshimaaAhmed <alshimaa185@gmail.com>
2024-02-10 15:04:12 +0200

    new

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """ constructor.
+        attribute:
+          size (int): the size of square"""
+        self.__size = size
